e7894f405dc73f4beee543422457a9d3c4e56ae8
BlumenKopf <c.brandau@gmail.com>
2021-01-14 20:50:43 +0100

    change UI

--- src/main_ui.py
+++ src/main_ui.py
@@ -11,7 +11,7 @@ if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
 if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
     PyQt5.QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
 
-class PageOne(QWidget):    
+class PageOne(QTabWidget):    
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -24,52 +24,89 @@ class PageOne(QWidget):
         self.setWindowIcon(QtGui.QIcon(QtGui.QPixmap(os.path.join('icons','Celos.bmp'))))
         self.setWindowTitle('Job Information')
         self.setFont(QtGui.QFont('Arial', 10))
-        
-        self.jobnamelbl = QLabel('Job Name:', self)
+
+        self.tab1 = QWidget(self)
+        self.tab2 = QWidget(self)
+
+        # Tab 1 
+        self.addTab(self.tab1, 'Tab 1')
+
+        self.jobnamelbl = QLabel('Job Name:', self.tab1)
         self.jobnamelbl.setFixedSize(100, 30)
         self.jobnamelbl.move(10, 10)
+        font = QtGui.QFont()
+        font.setUnderline(True)
+        self.jobnamelbl.setFont(font)        
 
-        self.jobnametxt = QLineEdit(self)
+        self.jobnametxt = QLineEdit(self.tab1)
         self.jobnametxt.setFixedSize(200, 30)
         self.jobnametxt.move(10, 40)
 
-        self.jobidlbl = QLabel('Job ID:', self)
+        self.jobidlbl = QLabel('Job ID:', self.tab1)
         self.jobidlbl.setFixedSize(100, 30)
         self.jobidlbl.move(250, 10)
+        font = QtGui.QFont()
+        font.setUnderline(True)
+        self.jobidlbl.setFont(font)
 
-        self.jobidtxt = QLineEdit(self)
+        self.jobidtxt = QLineEdit(self.tab1)
         self.jobidtxt.setFixedSize(200, 30)
         self.jobidtxt.move(250, 40)
 
-        self.date = QDateEdit(self)
-        self.date.move(10,80)
+        self.custnamelbl = QLabel('Customer Name:', self.tab1)
+        self.custnamelbl.move(10, 80)
+        font = QtGui.QFont()
+        font.setUnderline(True)
+        self.custnamelbl.setFont(font)
+        
+        self.custnametxt = QLineEdit(self.tab1)
+        self.custnametxt.setFixedSize(200, 30)
+        self.custnametxt.move(10, 105)
+
+        self.custidlbl = QLabel('Customer ID:', self.tab1)
+        self.custidlbl.move(250, 80)
+        font = QtGui.QFont()
+        font.setUnderline(True)
+        self.custidlbl.setFont(font)
+
+        self.custidtxt = QLineEdit(self.tab1)
+        self.custidtxt.setFixedSize(200, 30)
+        self.custidtxt.move(250, 105)
+
+        self.date = QDateEdit(self.tab1)
+        self.date.move(10,200)
         now = datetime.datetime.today()
         self.date.setDate(now)
 
-        self.time = QTimeEdit(self)
-        self.time.move(135,80)        
+        self.time = QTimeEdit(self.tab1)
+        self.time.move(135,200)        
 
-        self.button1 = QPushButton('Button', self)
+        self.button1 = QPushButton('Button', self.tab1)
         self.button1.move(10, 300)
         self.button1.setText('Next')
         self.button1.clicked.connect(self.next)
 
-        self.button2 = QPushButton('Button', self)
+        self.button2 = QPushButton('Button', self.tab1)
         self.button2.move(100, 300)
         self.button2.setText('Cancel')
         self.button2.clicked.connect(self.btn_close) 
 
-        self.combobox = QComboBox(self)
+        self.combobox = QComboBox(self.tab1)
         self.combobox.addItems([
             'Heidenhain',
             'Siemens',
             'Mapps'
         ])
-        self.combobox.move(10, 200)        
+        self.combobox.move(10, 400) 
+
+        #Tab 2
+        self.addTab(self.tab2, 'Tab 2')   
+
+
 
     def next(self):
-        pass
-    
+        self.setCurrentWidget(self.tab2)
+
     def btn_close(self):
         self.close()
     
